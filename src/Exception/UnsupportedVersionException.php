608ff1b48e1e8418220729de8d945b80b6dfb9ae
Anton Semenov <20430159+trydalcoholic@users.noreply.github.com>
2025-07-19 07:10:21 +0300

    feat(exception): add exception directory structure
    Add dedicated directory for custom exception classes
    to organize error handling components.
    
    Signed-off-by: Anton Semenov <20430159+trydalcoholic@users.noreply.github.com>

---
+++ src/Exception/UnsupportedVersionException.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) 2025 Anton Semenov <20430159+trydalcoholic@users.noreply.github.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cartwryte\Sleuth\Exception;
+
+use RuntimeException;
+
+/**
+ * Thrown when OpenCart version is not supported
+ */
+class UnsupportedVersionException extends RuntimeException {}
